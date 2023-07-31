8a3e05506eff6758b02070c4df2d459d7eed1456
Michael Li <36088648+magicalpuffin@users.noreply.github.com>
2023-07-31 00:13:07 -0700

    moved powershell and lua scripts

---
+++ ConvertHTMLToMarkdown.ps1
@@ -0,0 +1,21 @@
+# Set the path to the folder containing the HTML files
+$sourceFolder = "./confluence_workspace"
+
+# Set the path to the folder containing generated Markdown files
+$destinationFolder = "./markdown"
+
+# Set the path to the lua filter script
+$luaFilterPath = "filter_html.lua"
+
+# Get all HTML files in the folder
+$htmlFiles = Get-ChildItem -Path $sourceFolder -Filter "*.html" -File
+
+# Loop through each HTML file and convert to Markdown using Pandoc
+# foreach ($htmlFile in $htmlFiles[0..10]) {
+foreach ($htmlFile in $htmlFiles) {
+    $htmlFilePath = $htmlFile.FullName
+    $markdownFileName = Join-Path -Path $destinationFolder -ChildPath ([System.IO.Path]::ChangeExtension($htmlFile.Name, "md"))
+   
+    $command = "pandoc '$htmlFilePath' -f html -t gfm -L '$luaFilterPath' -s -o '$markdownFileName'"
+    Invoke-Expression $command
+}
---
+++ filter_html.lua
@@ -0,0 +1,55 @@
+function Span(el)
+  return el.content
+end
+  
+function Div(el)
+  -- Removes footer
+  if el.identifier == "footer" then
+    return {}
+  end
+  -- Removes breadcrumb header
+  if el.identifier == "breadcrumb-section" then
+    return {}
+  end
+  -- Removes attachments plugin
+  if el.classes:includes("plugin_attachments_container") then
+    return {}
+  end
+  return el.content
+end
+
+function Para(el)
+  if el.content then
+    for _, item in ipairs(el.content) do
+      -- Out of order execution, converted links should not be stringified
+      if item.t == "RawInline" then
+        return el
+      end
+    end
+    return pandoc.utils.stringify(el.content)
+  end
+end
+
+function Image(el)
+  return {}
+end
+
+function Link(el)
+  -- if el.classes:find("download-all-link", 0) then
+  --   return {}
+  -- end
+  if el.classes:includes("external-link") then
+    link_text = pandoc.utils.stringify(el.content)
+    link_url = el.target
+    markdown_link = string.format("[%s](%s)", link_text, link_url)
+
+    return {pandoc.RawInline("gfm", markdown_link)}
+  end
+end
+
+-- function Table(el)
+--   if el.classes:find("attachments", 0) then
+--     return {}
+--   end
+-- end
+  
